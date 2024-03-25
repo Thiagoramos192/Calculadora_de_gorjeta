075107f6079bdbb297cde6b35558b6c3137f5374
thiagoramos <thiago.rlima@eaporta.org>
2024-03-25 20:45:28 -0300

    nono commit do CSS e terceiro commit do js

--- scripty.js
+++ scripty.js
@@ -1,44 +1,28 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
 
-const containput = document.querySelector("#conta")
-containput.addEventListener("input",receberValorConta )
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta)
 
-function receberValorConta(evento){
-    conta = Number(evento.target.value)
-
+function receberValorConta(evento) {
+  conta = Number(evento.target.value)
 }
 
 const pessoasInput = document.querySelector("#pessoas")
-
 pessoasInput.addEventListener("input", receberQuantidadePessoa)
 
-function receberQuantidadePessoa (evento){
-    const paragrafoErro = document.querySelector(".pessoas #erro")
-    const divErro = document.querySelector(".pessoas .input-box")
+function receberQuantidadePessoa(evento) {
+  const paragrafoErro = document.querySelector(".pessoas #erro")
+  const divErro = document.querySelector(".pessoas .input-box")
 
-    if(evento.target.value === "0"){
-
-       console.log(paragrafoErro)
-       paragrafoErro.style.display = "block"
-
-       divErro.setAttribute("id", "erro-div")
-    } else{
-        paragrafoErro.style.display = "none"
-        divErro.setAttribute("id", "")
-        pessoas = Number (evento.target.value)
-    }
-} 
-
-const botoesGorjeta = document.querySelectorAll(".gorgeta input[type='button']")
-
-botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click",ReceberPorcentagem )
-})
-
-function ReceberPorcentagem(evento){
-    porcentagem = parseFloat(evento.target.value) /100
+  if (evento.target.value === "0") {
+    paragrafoErro.style.display = "block"
+    divErro.setAttribute("id", "erro-div")
+  } else {
+    paragrafoErro.style.display = "none"
+    divErro.setAttribute("id", "")
+    pessoas = Number(evento.target.value)
+  }
 }
-
-console.log(paragrafoErro)
